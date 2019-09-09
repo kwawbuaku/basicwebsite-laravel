15e7ed416dd27a423876c222908c19bd7c472cc3
David Buaku <davidbuaku@yahoo.com>
2019-09-09 10:55:27 +0100

    Refactored the route to a new controller

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('', 'PagesController@getHome');
+Route::get('about', 'PagesController@getAbout');
+Route::get('contact', 'PagesController@getContact');
 
-Route::get('about', function () {
-    return view('about');
-});
-
-Route::get('contact', function () {
-    return view('contact');
-});
 
 Route::post('/contact/submit', 'MessagesController@submit');
 Route::get('/messages', 'MessagesController@getMessages');
