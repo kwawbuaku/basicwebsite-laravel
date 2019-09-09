9492bfa2e78916b72393cc633bd3b0f7abc66cc1
David Buaku <davidbuaku@yahoo.com>
2019-09-09 10:05:56 +0100

    Created the contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
     <h1>Contact</h1>
+    {!! Form::open(['url' => 'contact/submit']) !!}
+    <div class="form-group">
+        {{  Form::label('name', 'Name') }}
+        {{ Form::text('name',null, ['placeholder' =>'Enter name', 'class' => 'form-control']) }}
+    </div>
+    <div class="form-group">
+        {{  Form::label('email', 'E-Mail Address') }}
+        {{ Form::text('email', null, ['placeholder' => 'example@gmail.com', 'class' => 'form-control']) }}
+{{--        {{ Form::text('email', 'example@gmail.com') }}--}}
+    </div>
+    <div class="form-group">
+        {{  Form::label('message', 'Message') }}
+        {{ Form::textarea('message', null, ['placeholder' => 'Enter message', 'class' => 'form-control']) }}
+        {{--        {{ Form::text('email', 'example@gmail.com') }}--}}
+    </div>
+    <div>
+        {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
+    </div>
+    {!! Form::close() !!}
 @endsection
