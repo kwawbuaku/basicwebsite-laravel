7b09b8a42f246fdd158d05d71c9d79b77df17a2a
David Buaku <davidbuaku@yahoo.com>
2019-09-09 11:00:49 +0100

    Updated the navbar so that the link is active when on the page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,13 +7,13 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+                <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact</a>
+                <a class="nav-link {{ Request::is('contact') ? 'active' : '' }}" href="/contact">Contact</a>
             </li>
         </ul>
     </div>
