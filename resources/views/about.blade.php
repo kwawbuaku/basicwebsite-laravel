f9982dc554dbe5bf0a4383c878fb995ade191bcc
David Buaku <davidbuaku@yahoo.com>
2019-09-09 08:35:31 +0100

    Added content and styling to the pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cupiditate et, explicabo impedit ipsa iusto
+        optio, ratione rem rerum totam ullam voluptatum. A cupiditate et fugit laboriosam placeat rem velit.</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur assumenda cumque error, esse excepturi
+        magni nam obcaecati, officia rerum sapiente velit. Aliquam amet autem beatae consectetur, harum officia
+        quae?</p>
 @endsection
 
 @section('sidebar')
